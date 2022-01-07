cc19872ebb2413eab2d4210c06b4a2a60ab3a1fe
zfyrom <gabriele.pala00@outlook.com>
2022-01-07 13:51:28 +0100

    hehehehe
    hehehehehehe

---
+++ polls/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # /polls/
+    path('', views.index, name='index'),
+    # /polls/question_id
+    path('<int:question_id>/', views.detail, name="detail"),
+    # /polls/question_id/results
+    path('<int:question_id>/results/', views.results, name="results"),
+    # /polls/question_id/vote
+    path('<int:question_id>/vote', views.vote, name="vote"),
+]
